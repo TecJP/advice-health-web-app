28ce3222d4711d3dd3ee63bfc78d25b8b4a574cb
TecJP <tec.joao.freitas@gmail.com>
2022-12-21 07:07:55 -0300

    Changing the bg of sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,9 +3,9 @@ import { Button, Col, Container, Navbar, Row } from "react-bootstrap";
 
 export function Sidebar() {
   return (
-    <Navbar style={{width: '4.5rem', height: '100vh', zIndex: 0}} className="bg-black text-white">
+    <Navbar style={{width: '4.5rem', height: '100vh', zIndex: 0}} className="bg-white text-black">
       <Container>
-          <CaretDoubleRight size={26} />
+        
       </Container>
     </Navbar>
   )
